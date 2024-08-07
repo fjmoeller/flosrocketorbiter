8d53a0792c6fa895943fe8d2dff92adc4fff7e87
fjmoeller <flomodoyt@gmail.com>
2024-08-07 23:28:58 +0200

    add orbizer page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { OrbiterComponent } from './pages/orbiter/orbiter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: OrbiterComponent }
+];
---
+++ src/app/pages/orbiter/orbiter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orbiter',
+  standalone: true,
+  imports: [],
+  templateUrl: './orbiter.component.html',
+  styleUrl: './orbiter.component.sass'
+})
+export class OrbiterComponent {
+
+}
